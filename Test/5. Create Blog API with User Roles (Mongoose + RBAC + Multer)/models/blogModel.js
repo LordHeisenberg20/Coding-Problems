e4660c8877f1f475867ba7d57faafea8287c1e00
Lord Heisenberg <thelordheisenberg20@gmail.com>
2025-07-22 13:35:37 +0530

    Create blogModel.js

---
+++ Test/5. Create Blog API with User Roles (Mongoose + RBAC + Multer)/models/blogModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
